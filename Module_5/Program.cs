c32804a7e994e89e88a18934472ebdb3fa76ecea
kurilkoivan7@gmail.com <kurilkoivan7@gmail.com>
2024-05-10 09:11:20 +0600

    Заданиие 1 модуль 5

--- Module_5/Program.cs
+++ Module_5/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        (string Name, string[] Diches) User;
+        Console.WriteLine("Введите имя");
+        User.Name = Console.ReadLine();
+        User.Diches = new string[5];
+        for (int i = 0; i < User.Diches.Length; i++)
+        {
+            Console.WriteLine("Введите любимое блюдо {0}", i + 1);
+            User.Diches[i] = Console.ReadLine();
+
+        }
+        Console.ReadKey();
+    }
+}
+
