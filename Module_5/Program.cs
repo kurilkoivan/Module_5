6458a45c6fc90d48cb13b9cbb84133011fdc9af4
kurilkoivan7@gmail.com <kurilkoivan7@gmail.com>
2024-05-12 08:36:19 +0600

    5.2.8

--- Module_5/Program.cs
+++ Module_5/Program.cs
@@ -2,10 +2,9 @@
 
 class MainClass
 {
-
-    static string ShowColor(string username)
+    static string ShowColor(string username,int userage)
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        Console.WriteLine("{0}, {1} лет \nНапишите свой любимый цвет на английском с маленькой буквы", username, userage);
         var color = Console.ReadLine();
 
         switch (color)
@@ -38,11 +37,34 @@ class MainClass
         }
         return color;
     }
-
-    public static void Main(string[] args)
+    static int[] GetArrayFromConsole()
     {
+        var result = new int[5];
 
-
+        for (int i = 0; i < result.Length; i++) 
+        {
+            Console.WriteLine("ВВедите элемент массива{0}", i + 1);
+            result[i] = int.Parse(Console.ReadLine());
+        }
+        return result;
+    }
+    static int[] SortArray(int[] result)
+    {
+        int temp = 0;
+        for (int i = 0; i < result.Length; i++)
+        {
+            for (int j = i + 1; result.Length > j; j++)
+                if (result[i] > result[j])
+                {
+                    temp = result[i];
+                    result[i] = result[j];
+                    result[j] = temp;
+                }
+        }
+            return result;
+    }
+public static void Main(string[] args)
+    {
         var (name, age) = ("Евгения", 27);
 
         Console.WriteLine("Мое имя: {0}", name);
@@ -66,20 +88,18 @@ class MainClass
         Console.WriteLine("Ваше имя: {0}", anketa.name);
         Console.WriteLine("Ваш возраст: {0}", anketa.age);
 
-        Console.ReadKey();
+        
 
 
         var favcolors = new string[3];
         for (int i = 0; i < favcolors.Length; i++) 
         {
-            favcolors[i] = ShowColor(anketa.name);    
+            favcolors[i] = ShowColor(anketa.name,anketa.age);    
         }
         Console.WriteLine("Ваши любимые цвета");
         foreach (var color in favcolors) 
         {
             Console.WriteLine(color);
         }
-
     }
-
 }
